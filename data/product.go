431792b94a220043718d948aba79e974c7e74a2a
Neutral <pirogovde@mer.ci.nsu.ru>
2023-03-11 00:55:07 +0700

    added Product structure, and Products structure, which is a list of Product objects. Implemented GetProducts function which returns list of Products. Implemented ToJSON function for Products which returns the encoder

---
+++ data/product.go
@@ -0,0 +1,54 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+// Product defines structure for api product
+// JSON annotation can be used to make custom output
+// `json:"-"` will completely ignore the field in api
+type Product struct {
+	ID          int 		`json:"id"`
+	Name        string 	`json:"name"`
+	Description string 	`json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string	`json:"sku"`
+	CreatedOn   string	`json:"-"`
+	UpdatedOn   string	`json:"-"`
+	DeletedOn   string	`json:"-"`
+}
+
+type Products []*Product
+
+func(p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+
+func GetProducts() Products {
+	return ProductList
+}
+
+var ProductList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
