7f248e7579d8c0fae07c1cc3829d5bd8c69754d5
Neutral <pirogovde@mer.ci.nsu.ru>
2023-03-12 12:36:34 +0700

    implemented UpdateProduct, findProduct functions and added var ErrProductNotFound

--- data/product.go
+++ data/product.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -46,6 +47,29 @@ func getNextID() int {
 	return lp.ID + 1
 }
 
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+} 
+
+var ErrProductNotFound = fmt.Errorf("product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
+}
 
 var productList = []*Product{
 	&Product{
